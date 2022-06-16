ab9ee4a1d72c7aa4eed0f4c9605984a9a40f86ad
Felizardo Caculo <felizardo1987@gmail.com>
2022-06-17 00:14:47 +0100

    Alterando mensagem de alerta

--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -2,4 +2,4 @@ let portuguese = 'Olá, Seja bem vindo ao meu Portfólio, por favor Selecione o 
 
 let english = 'Hello, Welcome to my Portfolio, please go to the menu and select (English) if you want to see the content in english linguage.'
 
-alert(`${portuguese}\nClique o botão (ok) para Fechar Essa Menssagem! \n\n ${english} \nClick (ok) to close this message! `)
+alert(`${portuguese}\nClique o botão (ok) para Fechar Essa Menssagem! \n\n${english} \nClick (ok) to close this message! `)
