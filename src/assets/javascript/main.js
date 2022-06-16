296c4b449a26a18ed807b7b470887a561c4a5854
Felizardo Caculo <felizardo1987@gmail.com>
2022-06-17 00:05:39 +0100

    Gerando a página em pt-BR

---
+++ src/assets/javascript/main.js
@@ -0,0 +1,5 @@
+let portuguese = 'Olá, Seja bem vindo ao meu Portfólio, por favor Selecione o menú (Portugês) se deseja ver o conteúdo na língua Portuguesa.'
+
+let english = 'Hello, Welcome to my Portfolio, please go to the menu and select (English) if you want to see the content in english linguage.'
+
+alert(`${portuguese}\nSeleccione (ok) para Fechar Essa Menssagem! \n\n ${english} \nSelect (ok) to close this message! `)
