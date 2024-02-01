4fda49e039fb24ee23592cea06e82d1baab62ff5
FelizardoCaculo <felizardo1987@gmail.com>
2024-02-01 23:22:46 +0100

    erase ChatGPT link and info

---
+++ serviceWorker.js
@@ -0,0 +1,11 @@
+const myPortfolioFc = "my-portfolio-fc";
+const assets = [
+    "/",
+    "/src/assets/images/image9.webp",
+    "/src/assets/images/image18.webp",
+    "/src/assets/images/image20.webp",
+    "/src/assets/css/style.css",
+    "/src/assets/javascript/main.js",
+    "/index.html",
+    "/manifest.json"
+]
--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -1,5 +1,20 @@
-let portuguese = 'Olá, Seja bem vindo ao meu Portfólio, por favor Selecione o menú (Português) se deseja ver o conteúdo na língua Portuguesa.'
-
-let english = 'Hello, Welcome to my Portfolio, please go to the menu and select (English) if you want to see the content in english linguage.'
-
-alert(`${portuguese}\nClique o botão (ok) para Fechar Essa Menssagem! \n\n${english} \nClick (ok) to close this message! `)
+let portuguese = 'Olá, Seja bem vindo ao meu Portfólio, por favor Selecione o menú (Português) se deseja ver o conteúdo na língua Portuguesa.';
+let english = 'Hello, Welcome to my Portfolio, please go to the menu and select (English) if you want to see the content in english linguage.';
+alert(`${portuguese}\nClique o botão (ok) para Fechar Essa Menssagem! \n\n${english} \nClick (ok) to close this message! `);
+navigator.serviceWorker.register('/serviceWorker.js').then(registration => {
+    console.log('Service Worker resgistered');
+    if(registration.installing){
+        registration.installing.postMessageMessage('Hi from your installing page');
+    } else [
+        err => {
+            console.error('installing the worker failed!', err);
+        }
+    ]
+});
+if('serviceWorker' in navigator){
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/serviceWorker.js')
+            .then(res => console.log('Service is now registered'))
+            .catch(err => console.log('Service Worker not registered', err));
+    });
+}
