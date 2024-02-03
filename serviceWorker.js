c6cc35f0f98b7fe990e5f3929ff9674e86da96fe
FelizardoCaculo <felizardo1987@gmail.com>
2024-02-03 02:12:06 +0100

    service worker configured

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,10 +1,40 @@
-const myPortfolioFc = "my-portfolio-fc";
+const myPortfolioFc = "my_portfolio_fc";
 const assets = [
     "/",
     "/src/assets/images/image9.webp",
     "/src/assets/images/image18.webp",
     "/src/assets/images/image20.webp",
+    "/src/assets/icons/icon-72x72.png",
+    "/src/assets/icons/icon-96x96.png",
+    "/src/assets/icons/icon-72x72.png",
+    "/src/assets/icons/icon-128x128.png",
+    "/src/assets/icons/icon-144x144.png",
+    "/src/assets/icons/icon-152x152.png",
+    "/src/assets/icons/icon-192x192.png",
+    "/src/assets/icons/icon-384x384.png",
+    "/src/assets/icons/icon-512x512.png",
+    "/manifest.json",
     "/src/assets/css/style.css",
     "/src/assets/javascript/main.js",
-    "/index.html"
+    "/src/assets/pages/portuguese.html"
 ]
+this.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(myPortfolioFc).then((cache) => {
+            return cache.addAll(assets);
+        })
+    );
+});
+this.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request).then((response) => {
+                let responseClone = response.clone();
+                caches.open(myPortfolioFc).then((cache) => {
+                    cache.put(event.request, responseClone);
+                    return response;
+                })
+            });
+        })
+    );
+});
--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -1,6 +1,7 @@
 let portuguese = 'Olá, Seja bem vindo ao meu Portfólio, por favor Selecione o menú (Português) se deseja ver o conteúdo na língua Portuguesa.';
 let english = 'Hello, Welcome to my Portfolio, please go to the menu and select (English) if you want to see the content in english linguage.';
 alert(`${portuguese}\nClique o botão (ok) para Fechar Essa Menssagem! \n\n${english} \nClick (ok) to close this message! `);
+
 navigator.serviceWorker.register('/serviceWorker.js').then(registration => {
     console.log('Service Worker resgistered');
     if(registration.installing){
